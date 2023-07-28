ff07049ef62ce2d102e04af9765260bf8c95a430
Andrey <KoftAndrey@gmail.com>
2023-07-28 13:26:10 +0300

    edited after comments

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 'use strict';
 
+// создание HTML
 const paragraph = document.createElement('p');
-
 const input = document.createElement('input');
 input.type = 'text';
-input.addEventListener('input', () => {
-  setTimeout(() => {
-    paragraph.textContent = input.value;
-  }, 300);
-});
-
 document.body.append(input, paragraph);
+
+// функционал
+const showInput = () => paragraph.textContent = input.value;
+
+let timeout;
+
+const debounce = () => {
+  clearTimeout(timeout);
+  timeout = setTimeout(showInput, 300);
+};
+
+input.addEventListener('keyup', debounce);
 
